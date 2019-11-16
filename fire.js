a8364d921aa80d3681fa749d451d9adbe4252435
Jackie098 <carlosmiranda19122@gmail.com>
2019-11-15 22:26:42 -0300

    feat JS: Função para inserir intensidade máxima do fogo nos últimos pixels

--- fire.js
+++ fire.js
@@ -4,6 +4,7 @@ const fireHeight = 10;
 
 function start(){
     createFireDataStructure();
+    createFireSource();
     renderFire();
     console.log(firePixelsArray);
 }
@@ -42,6 +43,15 @@ function renderFire(){
     html += '</table>';
 
     document.querySelector('#fireCanvas').innerHTML = html;
+}
+
+function createFireSource(){
+    for(let column = 0; column <= fireWidth; column++){
+        const overflowPixelIndex = fireWidth * fireHeight;
+        const pixelIndex = (overflowPixelIndex - fireWidth) + column;
+
+        firePixelsArray[pixelIndex] = 36;
+    }
 }
 
 start();
